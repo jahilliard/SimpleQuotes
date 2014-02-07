10d2b17cac1b57bf983784aca7ce4da66281a65e
Justin Hilliard <justin.a.hilliard@gmail.com>
2014-02-06 19:48:51 -0500

    created database and migrated it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SimpleQuotes::Application.routes.draw do
+  resources :quotes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
